d3c2cdc4778729e387c82c1af4716e087cd74836
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-11-26 23:04:48 +0900

    feat : step 8 add path method

--- NetCoreWeb/Procademy_ASPNETCOREMVC/Program.cs
+++ NetCoreWeb/Procademy_ASPNETCOREMVC/Program.cs
@@ -1,6 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", (HttpContext context) =>
+{
+    string path = context.Request.Path;
+    string method = context.Request.Method;
+    return "Request path: " + path + " Http Method: " + method;
+});
 
 app.Run();
