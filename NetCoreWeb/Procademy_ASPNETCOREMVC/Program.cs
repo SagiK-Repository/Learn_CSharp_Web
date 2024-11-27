1fb51705ff2175ce4d6752000823a9e52142a924
박주형 <juhyung.park@mirero.co.kr>
2024-11-28 08:05:42 +0900

    feat : add step 10 http status code

--- NetCoreWeb/Procademy_ASPNETCOREMVC/Program.cs
+++ NetCoreWeb/Procademy_ASPNETCOREMVC/Program.cs
@@ -1,19 +1,31 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", (HttpContext context) =>
+app.Run(async (HttpContext context) =>
 {
     string path = context.Request.Path;
     string method = context.Request.Method;
     var userAgent = string.Empty;
-    
-    if (context.Request.Headers.ContainsKey("User-Agent"))
-        userAgent = context.Request.Headers["User-Agent"];
 
-    context.Response.Headers["Content-Type"] = "text/html";
-    context.Response.Headers["MyHeader"] = "Hello, World";
-    return "<h2>This is a Text response</h2>";
-    return "Request path: " + path + " Http Method: " + method + " User Agent: " + userAgent;
+    if (path == "/" || path == "/Home")
+    {
+        context.Response.Headers["Content-Type"] = "text/html";
+        context.Response.Headers["MyHeader"] = "Hello, World";
+
+        context.Response.StatusCode = 200;
+        await context.Response.WriteAsync("<h2>This is a Text response</h2>");
+    }
+    else if (path == "/Contact")
+    {
+        if (context.Request.Headers.ContainsKey("User-Agent"))
+            userAgent = context.Request.Headers["User-Agent"];
+
+        context.Response.StatusCode = 200;
+        await context.Response.WriteAsync("Request path: " + path + " Http Method: " + method + " User Agent: " + userAgent);
+    }
+    else
+    {
+        context.Response.StatusCode = 404;
+        await context.Response.WriteAsync("The page you are looking for is not found!");
+    }
 });
-
-app.Run();
