2ebdf47897a5c4a673be68aa7912290f5ebb5dfd
박주형 <juhyung.park@mirero.co.kr>
2024-11-28 13:45:54 +0900

    feat : add Step 27, 28 29 Route

--- NetCoreWeb/Procademy_ASPNETCOREMVC/Learning 3/Program.cs
+++ NetCoreWeb/Procademy_ASPNETCOREMVC/Learning 3/Program.cs
@@ -3,6 +3,14 @@ var app = builder.Build();
 
 app.UseRouting();
 
+app.Use(async (context, next) =>
+{
+    Endpoint endpoint = context.GetEndpoint()!;
+    if (endpoint != null)
+        await context.Response.WriteAsync(endpoint.DisplayName!);
+    await next();
+});
+
 app.UseEndpoints(endpoints =>
 {
     _ = endpoints.Map("/Home", async (context) =>
@@ -18,6 +26,23 @@ app.UseEndpoints(endpoints =>
     _ = endpoints.MapPost("/Product", async (context) =>
     {
         await context.Response.WriteAsync("A new Product created");
+    });
+
+    _ = endpoints.MapGet("/Product/{id:int}", async (context) =>
+    {
+        var id = context.Request.RouteValues["ID"];
+        
+        if (id != null)
+            await context.Response.WriteAsync("\n\nThis is product with ID : " + id);
+
+        await context.Response.WriteAsync("\n\nYour are in Product page");
+    });
+
+    _ = endpoints.MapGet("/Product/book/author/{authorname=john-smith}/{bookid=1}", async (context) =>
+    {
+        var bookId = Convert.ToInt32(context.Request.RouteValues["bookid"]);
+        var authorName = Convert.ToString(context.Request.RouteValues["authorname"]); 
+        await context.Response.WriteAsync($"\n\nThis is the book authored by {authorName}, book id: {bookId}" );
     });
 });
 
