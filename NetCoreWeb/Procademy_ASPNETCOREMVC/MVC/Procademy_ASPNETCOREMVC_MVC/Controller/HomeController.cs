725f24c7b624f2ae702cf0b2d9d62505848241cf
박주형 <juhyung.park@mirero.co.kr>
2024-11-29 11:16:47 +0900

    feat : step 35 mvc controller

---
+++ NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Controller/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Procademy_ASPNETCOREMVC_MVC.Controller;
+
+public class HomeController
+{
+    [Route("Home")]
+    public string Index() => "Welcome ASP.NET Core Application!";
+}
