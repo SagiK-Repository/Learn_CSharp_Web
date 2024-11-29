a431b1cdb90b41eca3bdf1dd8350ba5c5f75d728
박주형 <juhyung.park@mirero.co.kr>
2024-11-29 15:36:34 +0900

    feat : add step 43 local redirectresult

--- NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Controllers/StoreController.cs
+++ NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Controllers/StoreController.cs
@@ -14,7 +14,45 @@ namespace Procademy_ASPNETCOREMVC_MVC.Controllers
             return new RedirectToActionResult("CategoryNote", "Store", new { }, permanent: true);
         }
 
+        [Route("/LocalRedirect")]
+        public IActionResult LocalRedirect()
+        {
+            return new LocalRedirectResult("/category/note", permanent: true);
+        }
+
         [Route("/category/note")]
         public IActionResult CategoryNote() => Content("Note Page", "text/plain");
+    }
+
+    public class NoteController : Controller
+    {
+        // /Note?IsLogged=true&NoteId=200
+        [Route("/Notes")]
+        public IActionResult Notes()
+        {
+            int noteId = Convert.ToInt32(Request.Query["BookId"]);
+            bool isLogged = Convert.ToBoolean(Request.Query["IsLogged"]);
+            return new RedirectResult($"/category/note/{noteId}/{isLogged}");
+            return new RedirectResult("https://google.com");
+            return LocalRedirectPermanent($"/category/note/{noteId}/{isLogged}");
+        }
+
+        // /category/note/?IsLogged=true&NoteId=200
+        [Route("/category/note/{noteid}/{isLogged}")]
+        public IActionResult NoteCategory()
+        {
+            if (!Request.RouteValues.ContainsKey("noteid"))
+                return BadRequest("Book Id is not provided!");
+
+            int noteId = Convert.ToInt32(Request.Query["BookId"]);
+            if (noteId < 1 || noteId > 1000)
+                return NotFound("Book Id is not in Range of 1 to 1000!");
+
+            bool isLogged = Convert.ToBoolean(Request.Query["IsLogged"]);
+            if (!isLogged)
+                return StatusCode(Microsoft.AspNetCore.Http.StatusCodes.Status401Unauthorized);
+
+            return Content($"Logged = {isLogged}, NoteId = {noteId}", "text/plain");
+        }
     }
 }
