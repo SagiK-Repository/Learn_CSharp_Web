fb050162ec26fc73ab75bff1b1f70f79fb59a55c
박주형 <juhyung.park@mirero.co.kr>
2024-11-29 11:30:49 +0900

    feat : add step 36 multi route

--- NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Controller/HomeController.cs
+++ NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Controller/HomeController.cs
@@ -4,6 +4,20 @@ namespace Procademy_ASPNETCOREMVC_MVC.Controller;
 
 public class HomeController
 {
+    [Route("/")]
     [Route("Home")]
     public string Index() => "Welcome ASP.NET Core Application!";
+
+    [Route("About")]
+    public string About() => "You are in About Page!";
+
+    [Route("Contact-Us")]
+    public string Contact() => "You are in Contact Page!";
+}
+
+[Controller] // 접미사 안쓰는 경우 다음과 같이 Attribute로 정의
+public class Home
+{
+    [Route("/products/{id:int:min(1000):max(9999)}")]
+    public string Products(int id) => $"You are in Products({id}) Page!";
 }
