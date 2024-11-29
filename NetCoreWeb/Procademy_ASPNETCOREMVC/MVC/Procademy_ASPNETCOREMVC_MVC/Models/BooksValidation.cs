10eb4d3f9029c24b5c32219f24ef560ce1b5af48
박주형 <juhyung.park@mirero.co.kr>
2024-11-29 16:50:55 +0900

    feat : add step 48 model validation

--- NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Models/BooksValidation.cs
+++ NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Models/BooksValidation.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 
 namespace Procademy_ASPNETCOREMVC_MVC.Models;
 
@@ -9,4 +10,23 @@ public class BooksValidation
     public string Name { get; set; }
     [Required(ErrorMessage = "Author is a required field")]
     public string Author { get; set; }
+}
+
+public class BookValidation
+{
+    public int? BookID { get; set; }
+    [Required(ErrorMessage = "{0} is a required field")]
+    [Display(Name = "Book Name")]
+    [StringLength(10, MinimumLength = 4, ErrorMessage = "(0} must have chacters between {3} and {1}")]
+    [RegularExpression("*[a-zA-Z ]$", ErrorMessage = "Book name is not valid")]
+    public string BookName { get; set; }
+    [ValidateNever]
+    public string Author { get; set; }
+    [Range(0, 999.99, ErrorMessage = "{0} must be between (1} and (2)")]
+    public Decimal Price { get; set; }
+    [EmailAddress(ErrorMessage = "Email format is not valid")]
+    public string AuthorEmail { get; set; }
+    public string Password { get; set; }
+    [Compare("Password", ErrorMessage = "Password does not match ConfirmPassword")]
+    public string ConfirmPassword { get; set; }
 }
