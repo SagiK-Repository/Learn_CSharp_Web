223f3d9e6b9f818f70ebacf6f8c3f41ad5b2e9c8
박주형 <juhyung.park@mirero.co.kr>
2024-11-29 13:31:39 +0900

    feat : add step 40 action result

--- NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Controllers/FileController.cs
+++ NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Controllers/FileController.cs
@@ -14,7 +14,7 @@ public class FileController : Controller
     [Route("File/Pysical/Download-file")]
     public PhysicalFileResult PysicalFile()
     {
-        string path = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "wwwroot", "Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf");
+        string path = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "wwwroot", "Sample", "Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf");
         return new(path, "application/pdf");
         return PhysicalFile(path, "application/pdf"); // 동일한 결과
     }
@@ -22,8 +22,19 @@ public class FileController : Controller
     [Route("File/Byte/Download-file")]
     public FileContentResult ByteFile()
     {
-        byte[] byteData = System.IO.File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "wwwroot", "Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf"));
+        byte[] byteData = System.IO.File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "wwwroot", "Sample", "Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf"));
         return new(byteData, "application/pdf");
         return File(byteData, "application/pdf"); // 동일한 결과
+   }
+
+    [Route("ActionResult")]
+    public IActionResult FileActionResult()
+    {
+        string path = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "wwwroot", "Sample", "Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf");
+
+        if (System.IO.File.Exists(path))
+            return PhysicalFile(path, "application/pdf");
+
+        return Content("File you specified is not found!", "text/plain");
     }
 }
