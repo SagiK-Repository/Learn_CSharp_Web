271bbdfd9a9faa1efc501b481fd2caad29a94232
박주형 <juhyung.park@mirero.co.kr>
2024-11-29 13:22:10 +0900

    style : 코드 정렬

--- NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Controllers/FileController.cs
+++ NetCoreWeb/Procademy_ASPNETCOREMVC/MVC/Procademy_ASPNETCOREMVC_MVC/Controllers/FileController.cs
@@ -1,30 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace Procademy_ASPNETCOREMVC_MVC.Controllers
+namespace Procademy_ASPNETCOREMVC_MVC.Controllers;
+
+public class FileController : Controller
 {
-    public class FileController : Controller
+    [Route("File/Download-file")]
+    public VirtualFileResult Index()
     {
-        [Route("File/Download-file")]
-        public VirtualFileResult Index()
-        {
-            return new ("Sample/Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf","application/pdf");
-            return File("Sample/Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf", "application/pdf"); // 동일한 결과
-        }
+        return new ("Sample/Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf","application/pdf");
+        return File("Sample/Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf", "application/pdf"); // 동일한 결과
+    }
 
-        [Route("File/Pysical/Download-file")]
-        public PhysicalFileResult PysicalFile()
-        {
-            string path = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "wwwroot", "Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf");
-            return new(path, "application/pdf");
-            return PhysicalFile(path, "application/pdf"); // 동일한 결과
-        }
+    [Route("File/Pysical/Download-file")]
+    public PhysicalFileResult PysicalFile()
+    {
+        string path = Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "wwwroot", "Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf");
+        return new(path, "application/pdf");
+        return PhysicalFile(path, "application/pdf"); // 동일한 결과
+    }
 
-        [Route("File/Byte/Download-file")]
-        public FileContentResult ByteFile()
-        {
-            byte[] byteData = System.IO.File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "wwwroot", "Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf"));
-            return new(byteData, "application/pdf");
-            return File(byteData, "application/pdf"); // 동일한 결과
-        }
+    [Route("File/Byte/Download-file")]
+    public FileContentResult ByteFile()
+    {
+        byte[] byteData = System.IO.File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "wwwroot", "Machine_Learning_Operations_MLOps_Overview_Definition_and_Architecture.pdf"));
+        return new(byteData, "application/pdf");
+        return File(byteData, "application/pdf"); // 동일한 결과
     }
 }
