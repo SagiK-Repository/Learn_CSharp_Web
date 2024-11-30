05c90da11cdca3b26e7a3667377e2a5e67603aaa
SagiKPJH1021 <Juhyung1021@gmail.com>
2024-12-01 08:07:42 +0900

    feat : delete 분리

--- NetCoreWeb/BoostMyTool/Application/UseCase/DeleteClientCommand.cs
+++ NetCoreWeb/BoostMyTool/Application/UseCase/DeleteClientCommand.cs
@@ -1,5 +1,40 @@
-﻿namespace BoostMyTool.Application.UseCase;
+﻿using BoostMyTool.Application.Interfaces;
+using MediatR;
+using System.Data.SqlClient;
 
-public class DeleteClientCommand
+namespace BoostMyTool.Application.UseCase;
+
+public record DeleteClientRequest(string Id) : IRequest<DeleteClientResponse>
 {
+    public string GetQuery() => "DELETE FROM clients WHERE id=@id;";
+}
+public record DeleteClientResponse()
+{
+    public string GetReDirect() => "/Clients/Index";
+}
+public class DeleteClientHandler : IRequestHandler<DeleteClientRequest, DeleteClientResponse>
+{
+    private readonly ISettings _settings;
+    public DeleteClientHandler(ISettings setting)
+    {
+        _settings = setting;
+    }
+
+    public async Task<DeleteClientResponse> Handle(DeleteClientRequest request, CancellationToken cancellationToken)
+    {
+        try
+        {
+            using (SqlConnection connection = new SqlConnection(_settings.GetDBConnectionInfo()))
+            {
+                connection.Open();
+                using SqlCommand command = new(request.GetQuery(), connection);
+                command.Parameters.AddWithValue("@id", request.Id);
+                command.ExecuteNonQuery();
+            }
+        }
+        catch (Exception e)
+        { }
+
+        return new DeleteClientResponse();
+    }
 }
